9b6e7b09d9abefca30d598e4ed488f06f314ddb3
Isaac Shapira <fresheyeball@protonmail.com>
2023-09-13 16:00:47 -0600

    head

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -1,5 +1,5 @@
 import ./common-hadrian.nix {
   version = "9.7.20230912";
-  rev = "fc86f0e7618c53036b7c5d3b834b9eb811476c3c";
-  sha256 = "13rf1d27wdich0kmbds55by9vj3wz0v9clba9p8qpwz7x7wpcjz2";
+  rev = "236a134eab4c0a3aae30752a3d580c083f4e6b57";
+  sha256 = "0w1y4k24flnhmqj3xjg9w1s517r61k4c0app7amfs996xx2c7ywp";
 }
